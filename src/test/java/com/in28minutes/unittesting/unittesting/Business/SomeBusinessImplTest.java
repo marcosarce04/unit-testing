63a31627ffa0e3356c908f53359270096ca3a128
Marcos Arce <marcosarce04@gmail.com>
2021-03-26 16:38:09 -0300

    Added Mockito implementation.

--- src/test/java/com/in28minutes/unittesting/unittesting/Business/SomeBusinessImplTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/Business/SomeBusinessImplTest.java
@@ -4,6 +4,8 @@ import com.in28minutes.unittesting.unittesting.Data.SomeDataService;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class SomeBusinessImplTest {
 
@@ -17,17 +19,30 @@ class SomeBusinessImplTest {
     }
 
     class SomeDataServiceStub implements SomeDataService {
-
         @Override
         public int[] retrieveAllData() {
             return new int[] {1,2,3};
         }
     }
-
     @Test
     public void calculateSumUsingDataService_basic() {
         SomeBusinessImpl business = new SomeBusinessImpl();
         business.setSomeDataService(new SomeDataServiceStub());
+        int actualResult = business.calculateSumUsingDataService();
+        int expectedResult = 6;
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void calculateSumUsingDataService2_basic() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+
+        SomeDataService dataServiceMock = mock(SomeDataService.class);
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1,2,3});
+
+        business.setSomeDataService(dataServiceMock);
+
         int actualResult = business.calculateSumUsingDataService();
         int expectedResult = 6;
 
