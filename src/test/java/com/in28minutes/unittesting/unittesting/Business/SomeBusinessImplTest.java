daca6d5a3f9bbb96d42c405e1db837293b088acc
Marcos Arce <marcosarce04@gmail.com>
2021-03-26 16:48:50 -0300

    Refactored code.

--- src/test/java/com/in28minutes/unittesting/unittesting/Business/SomeBusinessImplTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/Business/SomeBusinessImplTest.java
@@ -1,6 +1,7 @@
 package com.in28minutes.unittesting.unittesting.Business;
 
 import com.in28minutes.unittesting.unittesting.Data.SomeDataService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,20 +34,26 @@ class SomeBusinessImplTest {
 
         assertEquals(expectedResult, actualResult);
     }
+    ////////////mocking///////////////////////////////////////
+
+    SomeBusinessImpl business = new SomeBusinessImpl();
+    SomeDataService dataServiceMock = mock(SomeDataService.class);
+
+    @BeforeEach
+    public void before() {
+        business.setSomeDataService(dataServiceMock);
+    }
 
     @Test
     public void calculateSumUsingDataService2_basic() {
-        SomeBusinessImpl business = new SomeBusinessImpl();
-
-        SomeDataService dataServiceMock = mock(SomeDataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {1,2,3});
+        assertEquals(6, business.calculateSumUsingDataService());
+    }
 
-        business.setSomeDataService(dataServiceMock);
-
-        int actualResult = business.calculateSumUsingDataService();
-        int expectedResult = 6;
-
-        assertEquals(expectedResult, actualResult);
+    @Test
+    public void calculateSumUsingDataService2_empty() {
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {});
+        assertEquals(0, business.calculateSumUsingDataService());
     }
 
 }
